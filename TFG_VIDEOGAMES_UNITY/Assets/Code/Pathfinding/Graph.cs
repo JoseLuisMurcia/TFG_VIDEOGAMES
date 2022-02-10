f3884e2659c7c245a1ab852d929d32bc8ec4e5be
Joselu99 <alaricoperchitaf10@gmail.com>
2022-02-10 22:14:01 +0100

    hehe new branch hehe

--- TFG_VIDEOGAMES_UNITY/Assets/Code/Pathfinding/Graph.cs
+++ TFG_VIDEOGAMES_UNITY/Assets/Code/Pathfinding/Graph.cs
@@ -2,12 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// An array of connections outgoing from the given node.
+// The graph class simply returns an array of connection objects for any node that is queried.
+// From these objects the end node and cost can be retrieved.
+// A simple implementation of this class would store the connections for each node and
+// simply return the list. Each connection would have the cost and end node stored in memory.
+
+// A more complex implementation might calculate the cost only when it is required, using
+// information from the current structure of the game level.
+// Notice that there is no interface for a Waypoint in this listing, because we don’t need to specify
+// one. In many cases it is sufficient just to give nodes a unique number and to use integers as
+// the data type. In fact, we will see that this is a particularly powerful implementation because
+// it opens up some specific, very fast optimizations of the A * algorithm
 public class Graph : MonoBehaviour
 {
-    
+
+    // Grafo debe contener todos los waypoints
+    [SerializeField] List<Waypoint> graph = new List<Waypoint>();
+
     void Start()
     {
-        
+        CreateGraph();
     }
 
     // Update is called once per frame
@@ -16,6 +31,17 @@ public class Graph : MonoBehaviour
         
     }
 
+    // Donde se almacenan las connections? En la clase Waypoint?
+    void CreateGraph()
+    {
+
+    }
+    // Crear método que sepa con qué está conectado un nodo. Se deberán crear las Connection cuando se vaya recorriendo el array.
+    // El grafo tiene que saber a partir de un nodo, con qué nodos está conectados y el coste de cada conexión.
+    //public Connection[] GetConnections(Waypoint fromNode)
+    //{
+
+    //}
 
 }
 
