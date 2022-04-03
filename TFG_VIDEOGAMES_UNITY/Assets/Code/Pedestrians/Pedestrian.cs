96f9d3c90cac2d80bce32b091d4c1e94770abd50
Joselu99 <alaricoperchitaf10@gmail.com>
2022-04-03 15:00:14 +0200

    agent rot

--- TFG_VIDEOGAMES_UNITY/Assets/Code/Pedestrians/Pedestrian.cs
+++ TFG_VIDEOGAMES_UNITY/Assets/Code/Pedestrians/Pedestrian.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Pedestrian : MonoBehaviour
 {
+    public Camera cam;
+    public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                //Vector3.Lerp();
+                //transform.LookAt(hit.point, Vector3.up);
+                StartCoroutine(LookAtTarget(hit.point));
+            }
+        }
+    }
+
+    IEnumerator LookAtTarget(Vector3 target)
+    {
+        float seconds = 0.5f;
+        float t = 0;
+        Quaternion initialRotation = transform.rotation;
+        Vector3 direction = target - transform.position;
+        transform.rotation = initialRotation;
+        while (t < 1)
+        {
+            Quaternion.Slerp(initialRotation, Quaternion.LookRotation(direction), t);
+            t += 0.1f;
+        }
+        agent.SetDestination(target);
+        yield return null;
     }
 }
