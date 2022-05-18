067b2b117bd0a8800a5fed5e6e593285aa272fe5
Joselu99 <alaricoperchitaf10@gmail.com>
2022-05-18 21:23:20 +0200

    hehe

--- TFG_VIDEOGAMES_UNITY/Assets/Code/SteeringBehavior/Signal.cs
+++ TFG_VIDEOGAMES_UNITY/Assets/Code/SteeringBehavior/Signal.cs
@@ -31,17 +31,47 @@ public class Signal : MonoBehaviour
     }
     private void CreateIntersectionPriorityTrigger()
     {
+        // Create one box in the middle of the road
         Vector3 boxPos = road.transform.position;        
         // Create the object
-        GameObject newGameObject = new GameObject("Signal Priority Trigger");
+        GameObject newGameObject = new GameObject("Middle Signal Priority Trigger");
         newGameObject.transform.position = boxPos;
         //newGameObject.transform.parent = gameObject.transform;
         // Create the boxCollider
         BoxCollider box = newGameObject.AddComponent<BoxCollider>();
         box.isTrigger = true;
-        box.size = new Vector3(5f, 2f, 2f);
+        box.size = new Vector3(4f, 2f, 4f);
 
         SignalTrigger trigger = newGameObject.AddComponent<SignalTrigger>();
         trigger.signal = this;
+
+        // Create anothe box at the beggining of the road
+        Vector3 startBoxPos = FindStartEntryNode() - transform.forward * 2f;
+        // Create the object
+        GameObject startTrigger = new GameObject("Start Signal Priority Trigger");
+        startTrigger.transform.position = startBoxPos;
+        //newGameObject.transform.parent = gameObject.transform;
+        // Create the boxCollider
+        BoxCollider startBox = startTrigger.AddComponent<BoxCollider>();
+        startBox.isTrigger = true;
+        startBox.size = new Vector3(3.5f, 2f, 3.5f);
+
+        SignalTrigger roadStartTrigger = startTrigger.AddComponent<SignalTrigger>();
+        roadStartTrigger.signal = this;
+    }
+
+    private Vector3 FindStartEntryNode()
+    {
+        // We want the furthest node from the signal
+        Vector3 pos1 = road.laneReferencePoints[0];
+        Vector3 pos2 = road.laneReferencePoints[road.laneReferencePoints.Count-1];
+
+        float distance1 = Vector3.Distance(pos1, transform.position);
+        float distance2 = Vector3.Distance(pos2, transform.position);
+
+        if (distance1 > distance2)
+            return pos1;
+
+        return pos2;
     }
 }
