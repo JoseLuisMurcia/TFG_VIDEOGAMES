e7d4140b00fbf6fd1eaf4894993376855f76be7e
José Luis Murcia Gámez <alaricoperchitaf10@gmail.com>
2023-03-26 21:38:42 +0200

    suburbs in voronoi go brrrrrrrrrrr

--- TFG_VIDEOGAMES_UNITY/Assets/Code/ProceduralGeneration/RegionHelper.cs
+++ TFG_VIDEOGAMES_UNITY/Assets/Code/ProceduralGeneration/RegionHelper.cs
@@ -100,6 +100,98 @@ namespace PG
             }
 
             // Define the suburbs
+            int suburbsMinNodes = 1000;
+            int suburbsMaxNodes = 2000;
+            List <VoronoiRegion> freeRegions = new List<VoronoiRegion>();
+            foreach(VoronoiRegion region in regions)
+            {
+                if (!mainDistrictRegions.Contains(region))
+                    freeRegions.Add(region);
+            }
+
+            // Can't have a main city district neighbour, for the first region,
+            bool found = false;
+            VoronoiRegion firstRegion = null;
+            while (!found)
+            {
+                firstId = Random.Range(0, freeRegions.Count);
+                firstRegion = freeRegions[firstId];
+                if (IsTwoRegionsApartFromType(firstRegion, Region.Main))
+                    found = true;
+            }
+            // Initial region found, now iterate through neighbours
+            conditionsMet = false;
+            List<VoronoiRegion> firstSuburbsRegion = new List<VoronoiRegion>
+            {
+                firstRegion
+            };
+            candidatesIds = new HashSet<int>
+            {
+                firstId
+            };
+            nodeCount = 0;
+            while (!conditionsMet)
+            {
+                List<VoronoiRegion> neighbourRegions = firstSuburbsRegion[firstSuburbsRegion.Count - 1].neighbourRegions.ToList();
+                var mainDistrictRegionsInNeighbours = neighbourRegions.FindAll(region => region.regionType == Region.Main);
+                // always unable to add a region if it has a main district neighbour.
+                foreach (VoronoiRegion region in mainDistrictRegionsInNeighbours)
+                    neighbourRegions.Remove(region);
+
+                // Get the best candidate from the current neighbour
+                // Compare the matches with all the firstSuburbsRegion
+                VoronoiRegion bestCandidate = null;
+                int bestHits = -1;
+                foreach (VoronoiRegion candidate in neighbourRegions)
+                {
+                    int currentHits = 0;
+
+                    foreach (VoronoiRegion addedRegions in firstSuburbsRegion)
+                    {
+                        if (addedRegions.neighbourRegions.Contains(candidate))
+                        {
+                            currentHits++;
+                        }
+                    }
+                    if (currentHits > bestHits && CanBeAdded(suburbsMaxNodes, nodeCount, candidate) && !candidatesIds.Contains(candidate.id))
+                    {
+                        bestCandidate = candidate;
+                        bestHits = currentHits;
+                    }
+                }
+
+                candidatesIds.Add(bestCandidate.id);
+
+                firstSuburbsRegion.Add(bestCandidate);
+                nodeCount += bestCandidate.nodes.Count;
+                conditionsMet = nodeCount >= suburbsMinNodes ? true : false;
+            }
+            foreach (VoronoiRegion region in firstSuburbsRegion)
+            {
+                region.regionType = Region.Suburbs;
+                AddRegionToDistrict(region, Region.Suburbs);
+            }
+
+            // Están cogiendo de vecinos a regiones con distrito main
+            // Hay que modulizar el código, debe estar en funciones que hagan eso mejor.
+        }
+        private bool IsTwoRegionsApartFromType(VoronoiRegion region, Region regionType)
+        {
+            List<VoronoiRegion> neighbours = region.neighbourRegions.ToList();
+            VoronoiRegion desiredTypeRegion = neighbours.Find(neighbour => neighbour.regionType == regionType);
+            if (desiredTypeRegion != null)
+                return false;
+
+            // No first neighbour was of the exluded type, now look at the exterior neighbours
+            foreach(VoronoiRegion neighbour in neighbours)
+            {
+                // Look at all the neighbours in the selected region
+                List<VoronoiRegion> exteriorNeighbours = neighbour.neighbourRegions.ToList();
+                desiredTypeRegion = exteriorNeighbours.Find(neighbour => neighbour.regionType == regionType);
+                if (desiredTypeRegion != null)
+                    return false;
+            }
+            return true;
         }
         private void AddRegionToDistrict(VoronoiRegion region, Region regionType)
         {
