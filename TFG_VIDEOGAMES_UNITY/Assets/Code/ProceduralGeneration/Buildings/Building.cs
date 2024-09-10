b493183e317deb695e9f5ca76a6881371f848511
José Luis Murcia Gámez <alaricoperchitaf10@gmail.com>
2024-09-10 22:19:26 +0200

    Add and configure prefabs for each region
    Now they all have a buildingInfo script attached to know the size they require in the grid

--- TFG_VIDEOGAMES_UNITY/Assets/Code/ProceduralGeneration/Buildings/Building.cs
+++ TFG_VIDEOGAMES_UNITY/Assets/Code/ProceduralGeneration/Buildings/Building.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Building : MonoBehaviour
+namespace PG
 {
-    public BuildingInfo buildingInfo;
-
-    [System.Serializable]
-    public class BuildingInfo
+    public class Building : MonoBehaviour
     {
-        public GameObject prefab;
-        public int width;  // How many grid nodes in the X direction
-        public int height; // How many grid nodes in the Y direction
+        public BuildingInfo buildingInfo;
+
+        [System.Serializable]
+        public class BuildingInfo
+        {
+            public GameObject prefab;
+            public int xValue;  // How many grid nodes in the X direction
+            public int yValue; // How many grid nodes in the Y direction
+        }
     }
 }
+
