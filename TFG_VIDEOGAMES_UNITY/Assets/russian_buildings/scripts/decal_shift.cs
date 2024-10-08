24b61faa12f921d8c8cf2e1d0f3a721b5efb1f5b
José Luis Murcia Gámez <alaricoperchitaf10@gmail.com>
2024-10-08 23:13:27 +0200

    Upgrade from Unity 2020 to Unity 2022 :smile_cat:

---
+++ TFG_VIDEOGAMES_UNITY/Assets/russian_buildings/scripts/decal_shift.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class decal_shift : MonoBehaviour
+{
+    public bool PressHereToMove = false;
+    public float LookDistance = 10.0f;
+    public float Bias = 0.05f;
+
+    private void Update()
+    {
+        if (PressHereToMove)
+        {
+            DoMoveDecal();
+            PressHereToMove = false;
+        }
+    }
+
+    private void DoMoveDecal()
+    {
+        // тут ваши действия
+        //Debug.Log("sss");
+        // transform.Translate(Vector3.forward * 1.0f);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformVector(Vector3.forward), out hit))
+        {
+            if (hit.distance < LookDistance)
+                {
+                transform.Translate(Vector3.forward * (hit.distance - Bias));
+                //print("Found an object - distance: " + hit.distance);
+            }
+        }
+
+
+
+    }
+}
